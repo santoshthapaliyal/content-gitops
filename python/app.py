0e18166c3d97b134e7d83440dc2849cf75e1ce63
gajendrag <77163253+gajendrag@users.noreply.github.com>
2021-01-27 19:54:13 +0530

    Update app.py

--- python/app.py
+++ python/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello GG 2 World!"
+    return "Hello GG 2 india!"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000)
